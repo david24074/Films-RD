d7dcb6bf2d39ef99773cc5d77c5dcf0e6d948c51
Slice <remco@familiedegroot.org>
2023-04-15 22:42:35 +0200

    Start on signup

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from src import app, db
 from flask import render_template, redirect, request, url_for, flash
 from flask_login import login_user, login_required, logout_user, current_user
 from src.models import User
-from src.forms import LoginForm
+from src.forms import LoginForm, SignupForm
 
 @app.route('/')
 def index():
@@ -11,11 +11,24 @@ def index():
 @app.route("/login", methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    if form.validate_on_submit():
+    if form.validate_on_submit():# If the validation fails there is no error
         user = User.query.filter_by(email=form.email.data).first()
         if user.check_password(form.password.data) and user is not None:
-            login_user(user)
+            login_user(user)# There is no message to tell the user that they're logged in
     return render_template('login.html', form=form)
+
+@app.route("/signup", methods=['GET', 'POST'])
+def register():
+    form = SignupForm()
+    if form.validate_on_submit():# If the validation fails there is no error
+        if not form.validate_email(form.email):
+            # Replace this with something that sends a message
+            return "TEMP"
+        user = User(form.email.data, form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        login_user(user)
+    return render_template('signup.html', form=form)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- src/forms.py
+++ src/forms.py
@@ -1,8 +1,20 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, PasswordField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo
+from src.models import User
 
 class LoginForm(FlaskForm):
     email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Wachtwoord', validators=[DataRequired()])
     submit = SubmitField('Inloggen')
+
+class SignupForm(FlaskForm):
+    email = StringField('Email address', validators=[DataRequired(),Email()])
+    password = PasswordField('Password', validators=[DataRequired(), EqualTo('pass_confirm', message='Passwords don\'t match!')])
+    pass_confirm = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Signup')
+
+    def validate_email(self, field):
+        if User.query.filter_by(email=field.data).first():
+            return False
+        return True
