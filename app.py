4f5a358ad2ff8b283825829f12af1b08a26db12b
David Meijer <davidmeijer282@gmail.com>
2023-04-15 13:08:07 +0200

    Database aangepast, link naar login laten werken

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import sqlite3
 app = Flask(__name__)
 
 @app.route('/')
-def home():
+def index():
     conn = sqlite3.connect('database.db')
     c = conn.cursor()
     c.execute('SELECT * FROM users')
@@ -13,6 +13,15 @@ def home():
     movies = c.fetchall()
     conn.close()
     return render_template('index.html', users=users, movies=movies)
+
+@app.route('/users')
+def login():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor()
+    c.execute('SELECT * FROM users')
+    users = c.fetchall()
+    conn.close()
+    return render_template('login.html', users=users)
 
 if __name__ == '__main__':
     app.run(debug=True)
